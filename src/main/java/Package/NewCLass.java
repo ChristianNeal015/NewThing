11e19e63c1c5c8cd29c6a9865bc44fb7851d4ae2
ChristianNeal015 <cjdaking030@gmail.com>
2020-01-10 13:23:21 -0500

    pushed

---
+++ src/main/java/Package/NewCLass.java
@@ -0,0 +1,26 @@
+package Package;
+
+public class NewCLass {
+    public static void main(String[] args) {
+
+        /*byte byteVal = 2;
+        double doubleVal = 6.0d;
+        short shortVal = 7;
+        int intVal = 7000;
+        long longVal = 6000000;
+        float floatVal = 3.0f;
+        char charVal = '\u0000';
+
+        float f = 3.14f;
+        long v = (long) f;
+        System.out.println(v);*/
+
+        String aaa = "F";
+        if(aaa.toLowerCase().equals("f")){
+            System.out.println("hi");
+        }
+        //System.out.println(aaa.toLowerCase());
+        //System.out.println(aaa.toUpperCase());
+    }
+
+}
